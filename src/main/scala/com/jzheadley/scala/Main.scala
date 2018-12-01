ec117e048e4255d98699a53609a74a7db9c7e8b8
jzheadley <headleyjz@vcu.edu>
2018-11-30 20:44:24 -0500

    converted to using my file parser because Jorge's doesn't seem to read my label correctly.... not sure why... Also fixed the knn error where i sorted with furthest nodes first... which caused me to pick the absolute worst class for the instance I was classifying

--- src/main/scala/com/jzheadley/scala/Main.scala
+++ src/main/scala/com/jzheadley/scala/Main.scala
@@ -5,7 +5,6 @@ import org.apache.spark.ml.linalg.{DenseVector, Vectors}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.storage.StorageLevel
 
 import scala.collection.mutable.ListBuffer
 
@@ -14,17 +13,11 @@ object Main {
   def main(args: Array[String]) {
     val smallDatasetPath = "/home/jzheadley/IdeaProjects/ScalaKNN/src/main/resources/small.arff"
     val mediumDatasetPath = "/home/jzheadley/IdeaProjects/ScalaKNN/src/main/resources/medium.arff"
-    val datasetPath = smallDatasetPath
+    var datasetPath = smallDatasetPath
+    //        datasetPath = mediumDatasetPath
+
     val log = Logger.getLogger(getClass.getName)
     log.info(datasetPath)
-
-    val schema = DataTypes.createStructType(
-      Array(
-        StructField("label", IntegerType, nullable = false),
-        StructField("features", ArrayType(DoubleType), nullable = false),
-        StructField("id", LongType)
-      )
-    )
 
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -34,34 +27,33 @@ object Main {
       .master("local[*]")
       .getOrCreate()
     log.info("Loading dataset")
-
-
     //    labelThroughSparkSQLTest(spark)
 
-    val dataDf = spark
-      .read
-      .format("org.apache.spark.ml.source.arff")
-      .load(datasetPath)
-      .withColumn("id", monotonically_increasing_id())
-      .persist(StorageLevel.MEMORY_AND_DISK)
-
-
-    //    dataDf.printSchema()
+    //    val dataDf = spark
+    //      .read
+    //      .format("org.apache.spark.ml.source.arff")
+    //      .load(datasetPath)
+    //      .withColumn("id", monotonically_increasing_id())
+    //      .persist(StorageLevel.MEMORY_AND_DISK)
+    val dataDf: DataFrame = readArffIntoDataFrame(spark, datasetPath)
+    dataDf.show(300)
+    dataDf.printSchema()
     val k = 3
     val startTime = System.nanoTime()
     val predictions = runKNNOnFullDataset(spark, dataDf, k)
-    predictions.show()
+    predictions.show(400)
     val accuracy = computeAccuracy(predictions)
     val diff = (System.nanoTime() - startTime) / 1000000.0
 
-    println(f"\nThe KNN classifier for ${dataDf.rdd.count} instances required $diff%01.4fms CPU time. Accuracy was $accuracy%01.4f\n")
+    // should get 79.17% on small dataset and 60.62% on medium
+    println(f"\nThe KNN classifier for ${dataDf.rdd.count} instances required $diff%01.4fms CPU time. Accuracy was $accuracy%01.2f%%\n")
 
     spark.sqlContext.clearCache()
     spark.stop()
   }
 
   def runKNNOnFullDataset(spark: SparkSession, data: DataFrame, k: Int): DataFrame = {
-    val predictions = ListBuffer[(Long, Double)]()
+    val predictions = ListBuffer[(Long, Int)]()
     import spark.implicits._
     val it = data.toLocalIterator()
     while (it.hasNext) {
@@ -71,18 +63,19 @@ object Main {
       val prediction = knn(spark, data.where(s"id!=$instanceId"), row.get(1).asInstanceOf[DenseVector], instanceId, k)
       predictions.append((instanceId, prediction))
     }
+    data.show(400)
+    predictions.toDF().show(400)
     data.join(predictions.seq.toDF("id", "prediction"), "id")
   }
 
-  def knn(spark: SparkSession, train: DataFrame, testFeatures: DenseVector, testInstanceId: Long, k: Int): Double = {
+  def knn(spark: SparkSession, train: DataFrame, testFeatures: DenseVector, testInstanceId: Long, k: Int): Int = {
     val distanceUDF = udf {
       features: DenseVector => Vectors.sqdist(testFeatures, features)
     }
     val distanced = train.withColumn("distance", distanceUDF(train("features")))
       // just take the labels column since its all we need now doing it here might make things faster since we're dealing with less data?
       .select(col("label"), col("distance"))
-      .sort(desc("distance")) // sort it with highest distances first
-    //    distanced.show()
+      .sort("distance") // sort it with lowest distances first
     vote(distanced, k)
   }
 
@@ -94,7 +87,7 @@ object Main {
     * @param k         the current k closest we're looking for
     * @return the most common class amongst the k closest
     */
-  def vote(distanced: DataFrame, k: Int): Double = {
+  def vote(distanced: DataFrame, k: Int): Int = {
     // because spark does magic this hopefully isn't incredibly slow... probably is anyway
     // is that groupby with an aggregate n^2 ? feel like it might be... good thing k isn't generally very large
     val labelCounts = distanced
@@ -108,7 +101,7 @@ object Main {
     else {
       labelCounts
         .first() // take the first row which should have the highest occurrence
-        .getDouble(0) // get the label from the row so we can return it as the prediction
+        .getInt(0) // get the label from the row so we can return it as the prediction
     }
   }
 
@@ -120,21 +113,35 @@ object Main {
    */
   def computeAccuracy(predictions: DataFrame): Double = {
     val correctPredictions = predictions.where("prediction == label")
-    correctPredictions.rdd.count.toFloat / predictions.rdd.count.toFloat
+    println(s"have ${correctPredictions.count()} correctPredictions")
+    println(s"have ${predictions.count()} total Predictions")
+    (correctPredictions.rdd.count.toFloat / predictions.rdd.count.toFloat) * 100.0
   }
+
 
   /*
    * I wrote this method when I couldn't get the arff library to work
    */
-  def readArffIntoDataFrame(spark: SparkSession, datasetPath: String, schema: StructType): DataFrame = {
+  def readArffIntoDataFrame(spark: SparkSession, datasetPath: String): DataFrame = {
+    val schema = DataTypes.createStructType(
+      Array(
+        StructField("label", IntegerType),
+        StructField("featuresSeq", ArrayType(DoubleType))
+      )
+    )
     val dataRdd = spark.sparkContext.textFile(datasetPath)
       .map(line => line.toString.split(",")) // split everything by ,'s
       .filter(line => line.length > 1) // get rid of the arff header information rows
-      .map(instance => {
-      Row(instance.take(instance.length - 1).map(_.toDouble), instance.last.toInt) // map it to 2 columns so we have class as a separate column.  Hopefully it will help me maybe?
-    })
+      .map(instance =>
+      Row(instance.last.toInt, instance.take(instance.length - 1).map(_.toDouble)) // map it to 2 columns so we have class as a separate column.  Hopefully it will help me maybe?
+    )
 
+    val vectorizeUDF = udf {
+      features: Seq[Double] => Vectors.dense(features.toArray)
+    }
     spark.createDataFrame(dataRdd, schema)
+      .withColumn("features", vectorizeUDF(col("featuresSeq")))
       .withColumn("id", monotonically_increasing_id)
+      .select("label", "features", "id")
   }
 }
